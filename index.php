6fb00bdf73d19e45f57888c3c98ebe6629244ac1
IderaoluwaOlamide <133761531+IderaoluwaOlamide@users.noreply.github.com>
2023-06-01 10:51:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<html>
+  <head><title>Home</title>
+   <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/all.min.css" rel="stylesheet">
+    <link href="css/webfonts.css" rel="stylesheet">
+  </head>
+<body>
+
+  <div class="row">
+   <div class="col-md-12" style="background-image:url('images/assets (44).png'); height:600px"><br><br>
+      <div class="row">
+       <div class="col-md-4">
+       </div>
+       <div class="col-md-5 blue">
+        <div class="row">
+          <div class="col-md-4">
+          </div>
+          <div class="col-md-4">
+        <span style="font-size:30px">Todo App</span>
+        </div>				
+        <div class="col-md-4">
+        <b> 20:08:47:PM</b><br>
+        </div>
+        </div><br><br>
+<center><input type="text" name="spc" placeholder="Please enter your task here" size="45"><input type="submit" value="Add Task" class="g"></center><br>
+      <div class="row">
+      <div class="col-md-1">
+      </div>
+      <div class="col-md-2">
+        <b>SNO</b><br>
+        1<br><br>
+        2<br><br>
+        3<br><br>
+        4<br><br>
+        5<br><br>
+        6
+      </div>
+      <div class="col-md-4">
+      <b>Task</b><br>
+      HTML<br><br>
+      CSS<br><br>
+      BOOTSTRAP<br><br>
+      JAVASCRIPT<br><br>
+      REACT<br><br>
+      PHYTHON
+      </div>
+      <div class="col-md-5">
+      <center><b>Action</b><br>
+       <input type="button" value="Update" class="pink"><input type="button" value="Delete" class="red"><br><br>
+       <input type="button" value="Update" class="pink"><input type="button" value="Delete" class="red"><br><br>
+       <input type="button" value="Update" class="pink"><input type="button" value="Delete" class="red"><br><br>   
+       <input type="button" value="Update" class="pink"><input type="button" value="Delete" class="red"><br><br>
+       <input type="button" value="Update" class="pink"><input type="button" value="Delete" class="red"><br><br></center>
+      </div>
+      <div class="col-md-4">
+      </div>
+      </div>
+      </div>
+      </div>
+   </div>
+  </div>
